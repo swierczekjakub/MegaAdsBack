a8ac0a2c586b1a361ed7b555fdc0165ce9477991
Jakub Świerczek <swierczekjakub1@gmail.com>
2022-05-30 18:58:50 +0200

    Added all EP-s.

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "express-async-errors";
 import {handleError} from "./utils/errors";
 import rateLimit from 'express-rate-limit';
+import { adRouter } from "./routers/ad.router";
 
 const app = express();
 
@@ -14,10 +15,9 @@ app.use(rateLimit({
     windowMs: 5 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
 }));
-// Routes...
-// app.get('/', async (req, res) => {
-//    throw new ValidationError('Daamn!');
-// });
+
+app.use('/ad', adRouter)
+
 app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
